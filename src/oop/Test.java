0fd487e51131a4f1c2ccafb6495a7c5f8949c84c
Lmyasdf <1901187412@qq.com>
2026-06-23 15:24:15 +0800

    feat:类模板、对象初始化练习

--- src/oop/Test.java
+++ src/oop/Test.java
@@ -9,5 +9,9 @@ public class Test {
         s1.height = 1.8;
         s1.weight = 70;
         System.out.println(s1.name);
+        System.out.println(s1.age);
+        System.out.println(s1.sex);
+        System.out.println(s1.height);
+        System.out.println(s1.weight);
     }
 }
