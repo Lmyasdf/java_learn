916d3bfd3a3e640c672cab06c002d7319332252c
Lmyasdf <1901187412@qq.com>
2026-06-23 16:50:34 +0800

    feat:学习构造器使用

---
+++ src/oop/constructor/Student2.java
@@ -0,0 +1,12 @@
+package oop.constructor;
+
+public class Student2 {
+    //无参数构造器
+    public Student2(){
+        System.out.println("无参数构造器");
+    }
+    //有参数构造器
+    public Student2(String s){
+        System.out.println("有参数构造器");
+    }
+}
---
+++ src/oop/constructor/Test.java
@@ -0,0 +1,8 @@
+package oop.constructor;
+
+public class Test {
+    public static void main(String[] args){
+        Student2 s1 = new Student2();
+        Student2 s2 = new Student2("张三");
+    }
+}
