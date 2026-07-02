f51a910a62bcf8c2ba59da3152480af9e955d974
Lmyasdf <1901187412@qq.com>
2026-07-02 15:37:58 +0800

    feat：学习并认识继承，学会使用extends关键字

---
+++ src/oop/inheritance/Text.java
@@ -0,0 +1,15 @@
+package oop.inheritance;
+
+public class Text {
+    public static void main(String[] args){
+        //子类对象是由子类和父类共同创建出来的
+        //子类可以继承父类的非private属性和方法
+        Teacher t = new Teacher();
+        t.setName("张三");
+        t.setSex('男');
+        t.setSkill("教学");
+        System.out.println(t.getName());
+        System.out.println(t.getSex());
+        System.out.println(t.getSkill());
+    }
+}
