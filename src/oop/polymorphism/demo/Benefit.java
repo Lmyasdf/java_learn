dadfa4db746431a9341ca8257e800dcfaf27518d
Lmyasdf <1901187412@qq.com>
2026-07-19 09:50:16 +0800

    feat:学习多态的两个好处 1.解耦合 2.父类类型形参可接收任意子类变量

---
+++ src/oop/polymorphism/demo/Benefit.java
@@ -0,0 +1,19 @@
+package oop.polymorphism.demo;
+
+public class Benefit {
+    //多态的好处：
+
+    public void main(String[] args) {
+        // 1.解耦合
+        Animal a = new Dog();//dog可以换成cat等等，后面代码不需要修改
+        System.out.println(a.talk());
+        Animal b = new Cat();
+        test(b);
+        test(a);
+
+    }
+    //2.父类类型变量作为参数可以接受任意子类对象
+    public void test(Animal a){
+        System.out.println(a.talk());
+    }
+}
