85d3b0aec180c6989169da6e8b7e551dfcfecd3e
Lmyasdf <1901187412@qq.com>
2026-07-24 22:07:52 +0800

    feat: 学习集合的遍历

---
+++ src/collection/CollectionTraversalDemo.java
@@ -0,0 +1,29 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CollectionTraversalDemo {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("S");
+
+        //1.迭代器遍历
+        Iterator<String> it = list.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        //2.for遍历
+        for(String s : list) {
+            System.out.println(s);
+        }
+
+        //3.Lambda表达式遍历
+        //list.forEach(s -> System.out.println(s));
+        list.forEach(System.out::println);
+    }
+}
