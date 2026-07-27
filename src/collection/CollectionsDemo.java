4527e4aa67d96195cfc9607d612507336cb49c45
Lmyasdf <1901187412@qq.com>
2026-07-27 21:56:51 +0800

    feat: 学习Collections类的方法

---
+++ src/collection/CollectionsDemo.java
@@ -0,0 +1,23 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsDemo {
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+
+        //1.Collections的方法批量加
+        Collections.addAll(list,"zhangsan","lisi","wangwu");
+        System.out.println(list);
+
+        //2.打乱顺序
+        Collections.shuffle(list);
+        System.out.println(list);
+
+        //3.排序
+        Collections.sort(list,(String a,String b) -> b.length() - a.length());
+        System.out.println(list);
+    }
+}
