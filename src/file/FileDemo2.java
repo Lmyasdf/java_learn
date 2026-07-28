13b287cce9a9c14000645c0e6efa7f11fa02c372
Lmyasdf <1901187412@qq.com>
2026-07-28 17:02:52 +0800

    feat: 结合递归制作文件查找器

---
+++ src/file/FileDemo2.java
@@ -0,0 +1,45 @@
+package file;
+
+import java.io.File;
+import java.util.Scanner;
+
+//递归查找文件
+public class FileDemo2 {
+    public static final String FILENAME = "dlei01.txt";//目标文件
+    public static final String DIR = "D:\\JavaLearn";//目标目录
+    public static void main(String[] args){
+//        File dir = new File(DIR);
+//        File file = new File(FILENAME);
+//        search(dir,file);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入要查找的文件名：（输入待查找文件名称）");
+        String filename = sc.nextLine();
+        System.out.println("请规定再哪个文件夹中查找：（输入查找文件夹路径）");
+        String dirpath = sc.nextLine();
+        if(dirpath.equals("C"))dirpath = "C:\\";
+        else if (dirpath.equals("D"))dirpath = "D:\\";
+        File dir = new File(dirpath);
+        File file = new File(filename);
+        search(dir,file);
+        new Scanner(System.in).nextLine();  // 等待用户按回车
+    }
+
+    public static void search(File dir, File filename){
+        if(dir == null || !dir.exists() || dir.isFile()){
+            return;
+        }
+        File[] files = dir.listFiles();
+        if(files != null && files.length > 0){
+            for(File f: files){
+                if(f.isFile() && f.getName().equals(filename.getName())){
+                    System.out.println("目标文件路径：" + f.getAbsolutePath());
+                    System.out.println("查找完成！按 Enter 键退出...");
+                    return;
+                }
+                else search(f,filename);
+            }
+        }
+        System.out.println("未找到目标文件");
+        System.out.println("查找完成！按 Enter 键退出...");
+    }
+}
