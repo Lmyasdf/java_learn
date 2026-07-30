de07e68825bf3df75d495eb58089bb3d3b8ca78d
Lmyasdf <1901187412@qq.com>
2026-07-30 22:46:36 +0800

    refactor: 匿名内部类优化

--- src/thread/CreateDemo2.java
+++ src/thread/CreateDemo2.java
@@ -3,17 +3,17 @@ package thread;
 //第二种创建方式，实现Runnable接口
 public class CreateDemo2 {
     public static void main(String[] args) {
-        Runnable r = new MyRunnable();
+        Runnable r = new Runnable(){
+            @Override
+            public void run(){
+                for(int i=1;i<=5;i++){
+                    System.out.println("子线程"+i);
+                }
+            }
+        };
         Thread t = new Thread(r);
         t.start();
         for(int i=1;i<=5;i++) System.out.println("主线程"+i);
     }
 }
-class MyRunnable implements Runnable{
-    @Override
-    public void run(){
-        for(int i=1;i<=5;i++){
-            System.out.println("子线程"+i);
-        }
-    }
-}
+
