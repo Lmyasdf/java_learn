63bfdf4fd1f3142cfaecd7b8bd7a97c07cb2d413
Lmyasdf <1901187412@qq.com>
2026-07-30 21:12:20 +0800

    feat: 学习线程类的创建，认识多线程

---
+++ src/thread/Createdemo1.java
@@ -0,0 +1,18 @@
+package thread;
+
+public class Createdemo1 {
+    //main方法本身是由一条主线程负责执行的
+    public static void main(String[] args){
+        Thread t1 = new Mythread();
+        t1.start();//启动线程必须start，而不是直接调用run
+        for(int i=1;i<=5;i++) System.out.println("主线程执行" + i);
+    }
+
+}
+//定义一个子类继承Thread,成为一个线程类
+class Mythread extends Thread{
+    @Override
+    public void run(){
+        for(int i=1;i<=5;i++) System.out.println("子线程执行" + i);
+    }
+}
