6ea69b891f2e0c491ea1ce3f23f736c313c1eef0
Lmyasdf <1901187412@qq.com>
2026-08-01 16:18:19 +0800

    feat: 学习解决线程问题的三种方式，同步代码块、同步方法、lock

--- src/thread/safe/Account.java
+++ src/thread/safe/Account.java
@@ -4,20 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Account {
     private String cardId;
     private double money;
+    private final Lock lk = new ReentrantLock();//保护锁对象
 
     public void drawMoney(double money) {
         String name = Thread.currentThread().getName();
-        if (money >= money) {
-            System.out.println(name + " 取钱成功");
-            this.money -= money;
-        } else {
-            System.out.println(name + " 取钱失败");
+        lk.lock();//上锁
+        try {
+            if (this.money >= money) {
+                System.out.println(name + " 取钱成功");
+                this.money -= money;
+
+            } else {
+                System.out.println(name + " 取钱失败");
+            }
+        } finally {
+            lk.unlock();//解锁
         }
+
     }
 }
